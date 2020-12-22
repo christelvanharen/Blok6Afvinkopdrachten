c2475272214dd8d123dc4780e39c183ab6aaae49
christelvanharen <cvanharen@outlook.com>
2020-12-22 11:21:32 +0100

    Afvink_4 af

---
+++ src/Afvink_4/GeneFinder.java
@@ -0,0 +1,120 @@
+package Afvink_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GeneFinder extends JFrame implements ActionListener {
+    private JTextArea textveld1;
+    private JScrollPane scrol1;
+    private JTextArea textveld2;
+    private JScrollPane scrol2;
+    private JTextArea textveld3;
+    private JScrollPane scrol3;
+    private JComboBox drop;
+    private JButton check;
+    private JTextArea uitkomst;
+    private JScrollPane scrol4;
+
+    public static void main(String[] args) throws InterruptedException {
+        GeneFinder frame = new GeneFinder();
+        frame.setSize(600, 600);
+        frame.createGui();
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+    private void createGui() throws InterruptedException {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Container window = getContentPane();
+        window.setLayout(new FlowLayout());
+
+        textveld1 = new JTextArea(10, 15);
+        window.add(textveld1);
+
+        scrol1 = new JScrollPane(textveld1);
+        scrol1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        window.add(scrol1);
+
+        textveld2 = new JTextArea(10, 15);
+        window.add(textveld2);
+
+        scrol2 = new JScrollPane(textveld2);
+        scrol2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        window.add(scrol2);
+
+        textveld3 = new JTextArea(10, 15);
+        window.add(textveld3);
+
+        scrol3 = new JScrollPane(textveld3);
+        scrol3.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        window.add(scrol3);
+
+        String[] opties = {"Overeenkomsten 1&2&3", "Overeenkomsten 1&2", "Overeenkomsten 1&3", "Overeenkomsten 2&3"};
+        drop = new JComboBox(opties);
+        window.add(drop);
+        drop.addActionListener(this);
+
+        check = new JButton("Check overeenkomst");
+        window.add(check);
+        check.addActionListener(this);
+
+        uitkomst = new JTextArea(15, 45);
+        window.add(uitkomst);
+
+        scrol4 = new JScrollPane(uitkomst);
+        scrol4.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        window.add(scrol4);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        String text1 = textveld1.getText();
+        String[] genen_1 = text1.split("\n");
+
+        Set<String> genen1 = new HashSet<>(Arrays.asList(genen_1));
+
+        String text2 = textveld2.getText();
+        String[] genen_2 = text2.split("\n");
+
+        Set<String> genen2 = new HashSet<>(Arrays.asList(genen_2));
+
+        String text3 = textveld3.getText();
+        String[] genen_3 = text3.split("\n");
+
+        Set<String> genen3 = new HashSet<>(Arrays.asList(genen_3));
+
+        switch (drop.getSelectedIndex()) {
+            case 0 -> {
+                genen1.retainAll(genen2);
+                genen1.retainAll(genen3);
+                String st = new String(String.valueOf(genen1));
+                st = st.replace("[", "").replace("]", "");
+                st = st.replaceAll(", ", "\n");
+                uitkomst.setText(st);
+            }
+            case 1 -> {
+                genen1.retainAll(genen2);
+                String st = new String(String.valueOf(genen1));
+                st = st.replace("[", "").replace("]", "");
+                st = st.replaceAll(", ", "\n");
+                uitkomst.setText(st);
+            }
+            case 2 -> {
+                genen1.retainAll(genen3);
+                String st = new String(String.valueOf(genen1));
+                st = st.replace("[", "").replace("]", "");
+                st = st.replaceAll(", ", "\n");
+                uitkomst.setText(st);
+            }
+            case 3 -> {
+                genen2.retainAll(genen3);
+                String st = new String(String.valueOf(genen2));
+                st = st.replace("[", "").replace("]", "");
+                st = st.replaceAll(", ", "\n");
+                uitkomst.setText(st);
+            }
+        }
+    }
+}
---
+++ src/Afvink_4/Hashmap.java
@@ -0,0 +1,111 @@
+package Afvink_4;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Hashmap {
+    public static void main(String[] args) {
+        HashMap<String, String> volledig_afkorting = new HashMap<String, String>();
+
+        volledig_afkorting.put("Alanine", "Ala");
+        volledig_afkorting.put("Arginine", "Arg");
+        volledig_afkorting.put("Asparagine", "Asn");
+        volledig_afkorting.put("Asparaginezuur", "Asp");
+        volledig_afkorting.put("Cysteïne", "Cys");
+        volledig_afkorting.put("Fenylalanine", "Phe");
+        volledig_afkorting.put("Glutamine", "Gln");
+        volledig_afkorting.put("Glutaminezuur", "Glu");
+        volledig_afkorting.put("Glycine", "Gly");
+        volledig_afkorting.put("Histidine", "His");
+        volledig_afkorting.put("Isoleucine", "Ile");
+        volledig_afkorting.put("Leucine", "Leu");
+        volledig_afkorting.put("Lysine", "Lys");
+        volledig_afkorting.put("Methionine", "Met");
+        volledig_afkorting.put("Proline", "Pro");
+        volledig_afkorting.put("Serine", "Ser");
+        volledig_afkorting.put("Threonine", "Thr");
+        volledig_afkorting.put("Tryptofaan", "Trp");
+        volledig_afkorting.put("Tyrosine", "Tyr");
+        volledig_afkorting.put("Valine", "Val");
+
+        HashMap<String, String> afkorting_letter = new HashMap<String, String>();
+
+        afkorting_letter.put("Ala", "A");
+        afkorting_letter.put("Arg", "R");
+        afkorting_letter.put("Asn", "N");
+        afkorting_letter.put("Asp", "D");
+        afkorting_letter.put("Cys", "C");
+        afkorting_letter.put("Phe", "F");
+        afkorting_letter.put("Gln", "Q");
+        afkorting_letter.put("Glu", "E");
+        afkorting_letter.put("Gly", "G");
+        afkorting_letter.put("His", "H");
+        afkorting_letter.put("Ile", "I");
+        afkorting_letter.put("Leu", "L");
+        afkorting_letter.put("Lys", "K");
+        afkorting_letter.put("Met", "M");
+        afkorting_letter.put("Pro", "P");
+        afkorting_letter.put("Ser", "S");
+        afkorting_letter.put("Thr", "T");
+        afkorting_letter.put("Trp", "W");
+        afkorting_letter.put("Tyr", "Y");
+        afkorting_letter.put("Val", "V");
+
+        HashMap<String, String> letter_volledig = new HashMap<String, String>();
+
+        letter_volledig.put("A", "Alanine");
+        letter_volledig.put("R", "Arginine");
+        letter_volledig.put("N", "Asparagine");
+        letter_volledig.put("D", "Asparaginezuur");
+        letter_volledig.put("C", "Cysteïne");
+        letter_volledig.put("F", "Fenylalanine");
+        letter_volledig.put("Q", "Glutamine");
+        letter_volledig.put("E", "Glutaminezuur");
+        letter_volledig.put("G", "Glycine");
+        letter_volledig.put("H", "Histidine");
+        letter_volledig.put("I", "Isoleucine");
+        letter_volledig.put("L", "Leucine");
+        letter_volledig.put("K", "Lysine");
+        letter_volledig.put("M", "Methionine");
+        letter_volledig.put("P", "Proline");
+        letter_volledig.put("S", "Serine");
+        letter_volledig.put("T", "Threonine");
+        letter_volledig.put("W", "Tryptofaan");
+        letter_volledig.put("Y", "Tyrosine");
+        letter_volledig.put("V", "Valine");
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Let op! Hoofdletter gevoelig!" + "\n" +
+                "Voer een eenletterige afkorting van een aminozuur " +
+                "in: ");
+        String str= sc.nextLine();
+        if (str.length()==1){
+            if (letter_volledig.get(str)!= null) {
+                System.out.println(volledig_afkorting.get(letter_volledig.get(str)));
+                System.out.println(letter_volledig.get(str));
+            } else {
+                System.out.println(str + " is geen aminozuur");
+            }
+
+        }
+        else if (str.length()==3){
+            if (afkorting_letter.get(str) != null) {
+                System.out.println(afkorting_letter.get(str));
+                System.out.println(letter_volledig.get(afkorting_letter.get(str)));
+            } else {
+                System.out.println(str + " is geen aminozuur");
+            }
+
+        }
+        else{
+            if (volledig_afkorting.get(str) != null) {
+                System.out.println(volledig_afkorting.get(str));
+                System.out.println(afkorting_letter.get(volledig_afkorting.get(str)));
+            } else {
+                System.out.println(str + " is geen aminozuur");
+            }
+
+        }
+    }
+}
